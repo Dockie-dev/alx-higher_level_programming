be0a76b20f897a635e479512aa8b8f4075959932
Dockie-dev <agbaidorcas@gmail.com>
2023-09-24 12:17:32 -0700

    my files

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,40 @@
-a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
+#!/usr/bin/python3
+"""Module for the Square class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A Rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate the area of the Rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """A Square class that inherits from Rectangle."""
 
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        self.integer_validator("size", size)
+        """Initialize a Square instance."""
         super().__init__(size, size)
-        self.__size = size
+
+    def __str__(self):
+        """Return a string representation of the Square."""
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+
+
+if __name__ == "__main__":
+    s = Square(13)
+    print(s)
+    print(s.area())
